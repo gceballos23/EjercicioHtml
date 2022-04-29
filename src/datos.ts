6c5da23c603fd23610e3d9d01897095fa6885c2f
gceballos23 <103612856+gceballos23@users.noreply.github.com>
2022-04-28 22:31:32 -0300

    calculo area no convierte el elemento html en number

--- src/datos.ts
+++ src/datos.ts
@@ -1,8 +1,17 @@
 let btnEnv = document.getElementById("btnEnviar");
-let datos = document.getElementById("datos");
-let rotulo = document.getElementById("rotulo");
-rotulo.innerHTML = "Ingrese un dato:";
-btnEnv.innerHTML = "Aceptar";
+let datoAltura = document.getElementById("datosAltura");
+let datoBase = document.getElementById("datosBase");
+let baseRotulo = document.getElementById("baseRotulo");
+let alturaRotulo = document.getElementById("alturaRotulo");
+baseRotulo.innerHTML = "Ingrese la Base del Rectangulo";
+alturaRotulo.innerHTML = "Ingrese la Altura del Rectangulo";
+
+let altura = parseInt(datoAltura.value, 10);
+let base = parseInt(datoBase.value, 10);
+let area: number = altura * base;
+
 btnEnv.addEventListener("click", () => {
-  console.log("el dato ingersado es", datos.value);
+  console.log("La Altura es ", datoAltura.value);
+  console.log("La Base es ", datoBase.value);
+  console.log("El area es", area);
 });
