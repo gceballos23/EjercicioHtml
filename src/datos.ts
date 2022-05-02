5af62a1ba0d00a879fc24ce78946773233c97fed
gceballos23 <103612856+gceballos23@users.noreply.github.com>
2022-05-02 19:30:56 -0300

    Borrado de variable Se borraron variables que no se utilizaron

--- src/datos.ts
+++ src/datos.ts
@@ -6,10 +6,6 @@ let alturaRotulo = document.getElementById("alturaRotulo");
 baseRotulo.innerHTML = "Ingrese la Base del Rectangulo";
 alturaRotulo.innerHTML = "Ingrese la Altura del Rectangulo";
 
-let altura: number = Number(datoAltura.value);
-let base: number = Number(datoBase.value);
-let area: number = altura * base;
-
 btnEnv.addEventListener("click", () => {
   console.log("La Altura es ", datoAltura.value);
   console.log("La Base es ", datoBase.value);
